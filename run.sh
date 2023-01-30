ec7dbc57adaebdf8845213a01ff10f76bbafeaff
DeejayRevok <seryi_one@hotmail.com>
2023-01-30 21:07:20 +0100

    Fix image name

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ nohup filebeat -e -c /etc/filebeat/filebeat.yml &
 nohup metricbeat -e -c /etc/metricbeat/metricbeat.yml &
 if [ "${DEVELOPMENT}" = "true" ]; then
     go run github.com/cosmtrek/air@latest
-else 
-    go run main.go 
+else
+    go run main.go
 fi
